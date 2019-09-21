39edd624b9210ebb26a793c31aee193eb325f293
dileep sai <dileepsai.60@gmail.com>
2019-09-21 01:34:51 -0700

    added pattern programming

---
+++ 1.pattern_programming/sample.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class sample{
+public static void main(String[] args){
+Scanner s=new Scanner(System.in);
+String str=s.next();
+int a=s.nextint();
+System.out.print(str +" "+a);
+}
+}
